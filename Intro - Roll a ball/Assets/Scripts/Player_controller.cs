a20c636b9ec5d7c1e8beb588589cad891e6890ad
Brian Vo <ovnairb@gmail.com>
2016-12-03 23:33:00 -0800

    Finished intro project
    added text input when collecting items

--- Intro - Roll a ball/Assets/Scripts/Player_controller.cs
+++ Intro - Roll a ball/Assets/Scripts/Player_controller.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player_controller : MonoBehaviour
 {
     public float speed;
+    public Text countText;
     private new Rigidbody rigidbody;
+    private int count;
+    public GameObject item;
 
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
     }
 
     private void FixedUpdate()
@@ -20,5 +26,22 @@ public class Player_controller : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rigidbody.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("item_Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+            if (count == item.transform.childCount)
+                countText.text = "You win!";
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
     }
 }
