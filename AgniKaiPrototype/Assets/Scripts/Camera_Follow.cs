c5545cae9e4f028ff5fef02708fe1eea3f718926
Brian Vo <ovnairb@gmail.com>
2017-02-24 20:40:05 -0800

    AgniKai prototype
    Top down RPG
    Terrain, Collision, Player sprites, character/camera movement

---
+++ AgniKaiPrototype/Assets/Scripts/Camera_Follow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour
+{
+
+    public Transform target_player;
+    public float m_speed = 0.1f;
+    private Camera camera;
+	// Use this for initialization
+	void Start ()
+	{
+	    camera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    camera.orthographicSize = (Screen.height / 100f);
+
+	    if (target_player)
+	    {
+	        transform.position = Vector3.Lerp(transform.position, target_player.position, m_speed) + new Vector3(0,0, -10);
+	    }
+	}
+}
---
+++ AgniKaiPrototype/Assets/Scripts/Player_movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_movement : MonoBehaviour
+{
+    private Rigidbody2D body;
+    private Animator anim;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    body = GetComponent<Rigidbody2D>();
+	    anim = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    Vector2 movement_vector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+	    if (movement_vector != Vector2.zero)
+	    {
+	        anim.SetBool("is_walking", true);
+	        anim.SetFloat("input_x", movement_vector.x);
+	        anim.SetFloat("input_y", movement_vector.y);
+
+	    }
+	    else
+	    {
+	        anim.SetBool("is_walking", false);
+	    }
+
+	    body.MovePosition(body.position + movement_vector * Time.deltaTime * 2);
+
+	}
+}
